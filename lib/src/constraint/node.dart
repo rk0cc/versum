0d2d129f70cf3cc5c29fd4aec3fd4878d09efd7d
Cyrus Chan <cyruschan1212@gmail.com>
2022-02-17 09:08:59 +0800

    Redesign node

--- lib/src/constraint/node.dart
+++ lib/src/constraint/node.dart
@@ -1,11 +1,6 @@
+import 'dart:collection';
+
 import 'package:meta/meta.dart' show sealed;
 import 'package:quiver/core.dart';
 
 import '../semver.dart' show SemVer;
-
-abstract class VersionConstraint<E extends Enum> {
-  String? get rawConstraint;
-  E get constraintPattern;
-  SemVer? get from;
-  SemVer? get to;
-}
