c2a5857bf28f8b006c1f7e3995adb02716913882
Cyrus Chan <cyruschan1212@gmail.com>
2022-02-24 20:59:20 +0800

    Add ignore prelease option

--- lib/src/constraints.dart
+++ lib/src/constraints.dart
@@ -22,8 +22,25 @@ enum VersionConstraintOperator {
   lower
 }
 
+bool _sameVersion(SemVer s1, SemVer s2) =>
+    s1.major == s2.major &&
+    s1.minor == s2.minor &&
+    s1.patch == s2.patch &&
+    s1.build == s2.build;
+
+/// Extension for [VersionConstraintOperator] which giving condition according
+/// to the operator.
 extension _VersionConstraintOperator on VersionConstraintOperator {
-  bool matched(SemVer constraint, SemVer provided) {
+  /// Check the [provided] one is matched [constraint]'s describe.
+  ///
+  /// If [excludePreRelease] set as `true`, it return `false` when [provided]
+  /// [SemVer] contains [SemVer.preRelease] but not mentioned in [constraint].
+  /// However, when [constraint] has non-nulled [SemVer.preRelease], it does not
+  /// affect the condition.
+  bool matched(SemVer constraint, SemVer provided, bool excludePreRelease) {
+    if (excludePreRelease &&
+        constraint.preRelease == null &&
+        _sameVersion(constraint, provided)) return false;
     switch (this) {
       case VersionConstraintOperator.greater:
         return constraint > provided;
@@ -36,6 +53,7 @@ extension _VersionConstraintOperator on VersionConstraintOperator {
     }
   }
 
+  /// Return a [String] which repersent the symbol of operator.
   String get symbol {
     switch (this) {
       case VersionConstraintOperator.greater:
@@ -50,15 +68,29 @@ extension _VersionConstraintOperator on VersionConstraintOperator {
   }
 }
 
+/// A record repersenting entire version constraint's metadata.
 mixin VersionConstraintRecord {
-  bool inRange(SemVer version);
+  /// Giving a [version] that is stastified with this record.
+  ///
+  /// If [excludePreRelease] set as `true`, the result will consider the given
+  /// [version]'s pre-release field is nulled already.
+  bool inRange(SemVer version, {bool excludePreRelease = true});
 }
 
+/// Extended [VersionConstraintRecord] which indicating which objects allows
+/// to assign in [OrMultipleVersionConstraint].
+///
+/// This mixin does not implemented additional functions and fields. Just uses
+/// to be indicated eligible type of [VersionConstraintRecord].
 mixin OrConstraintRecordSection on VersionConstraintRecord {}
 
+/// Smallest entity of [VersionConstraintRecord] that contains [SemVer] and
+/// [VersionConstraintOperator] only and enough to uses for giving constraint
+/// result.
 @sealed
 class VersionConstraintNode
     with VersionConstraintRecord, OrConstraintRecordSection {
+  /// Indicate a version to be affected.
   final SemVer affectedVersion;
   final VersionConstraintOperator constraintOperator;
 
@@ -69,8 +101,8 @@ class VersionConstraintNode
           required VersionConstraintOperator constraintOperator}) =>
       VersionConstraintNode._(affectedVersion, constraintOperator);
 
-  bool inRange(SemVer version) =>
-      constraintOperator.matched(affectedVersion, version);
+  bool inRange(SemVer version, {bool excludePreRelease = true}) =>
+      constraintOperator.matched(affectedVersion, version, excludePreRelease);
 
   String get traditionalVersionConstraint =>
       "${constraintOperator.symbol}${affectedVersion}";
@@ -95,8 +127,9 @@ class AndMultipleVersionConstraint
       : super._(constraints);
 
   @override
-  bool inRange(SemVer version) =>
-      _constraints.every((vcs) => vcs.inRange(version));
+  bool inRange(SemVer version, {bool excludePreRelease = true}) =>
+      _constraints.every(
+          (vcs) => vcs.inRange(version, excludePreRelease: excludePreRelease));
 }
 
 @sealed
@@ -108,8 +141,8 @@ class OrMultipleVersionConstraint
       : super._(constraints);
 
   @override
-  bool inRange(SemVer version) =>
-      _constraints.any((vcs) => vcs.inRange(version));
+  bool inRange(SemVer version, {bool excludePreRelease = true}) => _constraints
+      .any((vcs) => vcs.inRange(version, excludePreRelease: excludePreRelease));
 }
 
 abstract class VersionConstraint<B extends MultipleVersionConstraintNode> {
@@ -119,5 +152,7 @@ abstract class VersionConstraint<B extends MultipleVersionConstraintNode> {
   @protected
   VersionConstraint(this.rawConstraint, this.constraintsContainer);
 
-  bool stastified(SemVer version) => constraintsContainer.inRange(version);
+  bool stastified(SemVer version, {bool excludePreRelease = true}) =>
+      constraintsContainer.inRange(version,
+          excludePreRelease: excludePreRelease);
 }
--- lib/versum.dart
+++ lib/versum.dart
@@ -1,4 +1,5 @@
 library versum;
 
 export 'src/semver.dart';
-export 'src/constraints.dart';
+export 'src/constraints.dart'
+    hide VersionConstraintRecord, OrConstraintRecordSection;
